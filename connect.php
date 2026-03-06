519214c32baa9643b89290c4a6ddb6d3141ef1fa
avipatel1226 <avipatel1226@gmail.com>
2026-03-06 13:15:32 -0500

    errors fixed in database connection

--- connect.php
+++ connect.php
@@ -10,6 +10,6 @@ try {
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
+    die("Database ection failed: " . $e->getMessage());
 }
 ?>
